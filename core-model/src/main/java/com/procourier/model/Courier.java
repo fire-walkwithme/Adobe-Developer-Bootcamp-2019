10de1ed9c569fc99f6b06d4d338fdb4f9523fb85
Ruxandra Monorean <ruxandra.monorean@gmail.com>
2019-03-21 18:28:32 +0200

    Added data repo

--- core-model/src/main/java/com/procourier/model/Courier.java
+++ core-model/src/main/java/com/procourier/model/Courier.java
@@ -3,12 +3,15 @@ package com.procourier.model;
 import java.util.Objects;
 
 public class Courier {
+    private Long id;
     private String companyName;
     private Address headquarters;
     private Long pricePerDistance;
     private Long pricePerWeight;
 
-    public Courier(String companyName, Address headquarters, Long pricePerDistance, Long pricePerWeight) {
+    public Courier(Long id, String companyName, Address headquarters, Long pricePerDistance,
+                   Long pricePerWeight) {
+        this.id = id;
         this.companyName = Objects.requireNonNull(companyName);
         this.headquarters = Objects.requireNonNull(headquarters);
         this.pricePerDistance = Objects.requireNonNull(pricePerDistance);
--- core-model/src/main/java/com/procourier/model/Order.java
+++ core-model/src/main/java/com/procourier/model/Order.java
@@ -6,14 +6,16 @@ import java.util.Map;
 import java.util.Objects;
 
 public class Order {
+    private Long id;
     private Seller seller;
     private Buyer buyer;
     private Courier courier;
     private Instant submittedTime;
     private Map<Product, Integer> orderLines;
 
-    public Order(Seller seller, Buyer buyer, Courier courier,
+    public Order(Long id, Seller seller, Buyer buyer, Courier courier,
                  Instant submittedTime, Map<Product, Integer> orderLines) {
+        this.id = Objects.requireNonNull(id);
         this.seller = Objects.requireNonNull(seller);
         this.buyer = Objects.requireNonNull(buyer);
         this.courier = Objects.requireNonNull(courier);
@@ -21,8 +23,12 @@ public class Order {
         this.orderLines = Collections.unmodifiableMap(orderLines);
     }
 
-    public Order(Seller seller, Buyer buyer, Courier courier, Map<Product, Integer> orderLines) {
-        this(seller, buyer, courier, Instant.now(), orderLines);
+    public Order(Long id, Seller seller, Buyer buyer, Courier courier, Map<Product, Integer> orderLines) {
+        this(id, seller, buyer, courier, Instant.now(), orderLines);
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public Seller getSeller() {
